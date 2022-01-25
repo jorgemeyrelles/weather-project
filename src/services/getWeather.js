ed156dfe2a4f87aff074d32dba3afaed8fd21672
jorgemeyrelles <jotaengpuc@gmail.com>
2022-01-25 09:45:49 -0300

    fix: deploy

--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -1,5 +1,5 @@
 const fetchWeather = async (location) => {
-  const url = 'http://api.weatherapi.com/v1/current.json?key=';
+  const url = process.env.URL_WEATHER || 'http://api.weatherapi.com/v1/current.json?key=';
   const key = process.env.API_KEY_WEATHER || '47c4f19258244008ac6141552221901';
   const wth = await fetch(`${url}${key}&q=${location}&aqi=no`)
     .then((response) => response.json());
