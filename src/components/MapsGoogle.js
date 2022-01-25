a9f3432a3249ced71c8fd09f5550e09fff6e7565
jorgemeyrelles <jotaengpuc@gmail.com>
2022-01-25 14:00:16 -0300

    fix: dotenv

--- src/components/MapsGoogle.js
+++ src/components/MapsGoogle.js
@@ -20,7 +20,7 @@ function MapsGoogle() {
   // , loadError
   const { isLoaded } = useLoadScript({
     id: 'google-map-script',
-    googleMapsApiKey: process.env.API_KEY_MAPS || 'AIzaSyAcifhLvWUgJ_I7eGGWhsTJs4w16o0iK1k',
+    googleMapsApiKey: process.env.REACT_APP_API_KEY_MAPS,
     libraries,
   });
 
--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -1,6 +1,6 @@
 const fetchWeather = async (location) => {
-  const url = process.env.URL_WEATHER || 'http://api.weatherapi.com/v1/current.json?key=';
-  const key = process.env.API_KEY_WEATHER || '47c4f19258244008ac6141552221901';
+  const url = process.env.REACT_APP_URL_WEATHER;
+  const key = process.env.REACT_APP_API_KEY_WEATHER;
   const wth = await fetch(`${url}${key}&q=${location}&aqi=no`)
     .then((response) => response.json());
   return wth;
